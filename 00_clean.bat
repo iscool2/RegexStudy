c3b9149683758911d3026630aa6f9fde88fe4be3
YTLIM <iscool2@gmai.com>
2025-01-06 16:21:57 +0900

    v11.3.0.1 from office

--- 00_clean.bat
+++ 00_clean.bat
@@ -1,1 +1,2 @@
-Del *.obj;*.sbr;*.ncb;*.pch;*.aps;*.pdb;*.bsc;*.clw;*.plg;*.idb;*.ilk;*.res;*.sdf;*.ipch /s /q
+rd .vs /S /Q
+Del *.obj;*.sbr;*.ncb;*.pch;*.aps;*.pdb;*.bsc;*.clw;*.plg;*.idb;*.ilk;*.res;*.sdf;*.ipch;*.log;*.tlog;*.iobj;*.ipdb;*.recipe /s /q
